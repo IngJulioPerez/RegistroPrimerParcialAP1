9169f0964f882635ebdc2b1457efe93d9e4343a7
IngJulioPerez <60108127+IngJulioPerez@users.noreply.github.com>
2020-06-15 18:32:55 -0400

    Agregando las entidades.

---
+++ Entidades/Articulos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPrimerParcialAP1.Entidades
+{
+    public class Articulos
+    {
+        [Key]
+
+        public int ProductoID { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public float Costo { get; set; }
+        public float ValorInventario { get; set; }
+        
+    }
+}
