6987b3e38332fb9bcb32ee933b6d92f6cd08df23
IngJulioPerez <60108127+IngJulioPerez@users.noreply.github.com>
2020-06-15 18:45:46 -0400

    Agregando contexto y creando la base de datos.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RegistroPrimerParcialAP1.Entidades;
+
+namespace RegistroPrimerParcialAP1.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DbArticulos.db");
+        }
+
+    }
+}
